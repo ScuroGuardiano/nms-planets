8dde3b3504b383b78300cc5c2da6554cd7bfb1a7
ScuroGuardiano <scuroguardiano@gmail.com>
2022-07-03 22:08:16 +0000

    deleting records

--- packages/nms-front/src/app/resources/resource-list/resource-list.component.ts
+++ packages/nms-front/src/app/resources/resource-list/resource-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, DoCheck, OnInit, SimpleChanges } from '@angular/core';
+import { AlertService, NotificationService, NotificationStyleType } from '@swimlane/ngx-ui';
 import { IResource } from 'src/app/api-interfaces';
 import { ErrorHandlerService } from 'src/app/services/error-handler.service';
 import { ResourcesService } from 'src/app/services/resources.service';
@@ -8,11 +9,13 @@ import { ResourcesService } from 'src/app/services/resources.service';
   templateUrl: './resource-list.component.html',
   styleUrls: ['./resource-list.component.scss']
 })
-export class ResourceListComponent implements OnInit, DoCheck {
+export class ResourceListComponent implements OnInit {
 
   constructor(
     private resourcesService: ResourcesService,
-    private errorHandler: ErrorHandlerService
+    private errorHandler: ErrorHandlerService,
+    private notificationService: NotificationService,
+    private alertService: AlertService
   ) { }
 
   loadingPromise?: Promise<any>;
@@ -21,9 +24,6 @@ export class ResourceListComponent implements OnInit, DoCheck {
 
   ngOnInit(): void {
     this.loadData();
-  }
-  ngDoCheck() {
-    console.log("ResourceListComponent::ngDoCheck")
   }
 
   ngOnChanges(changes: SimpleChanges): void {
@@ -76,5 +76,59 @@ export class ResourceListComponent implements OnInit, DoCheck {
 
   get allSelected() {
     return this.selected.size == this.resources!.length;
+  }
+
+  async onDelete(resource: IResource) {
+    const confirmation = await this.alertService.confirm({
+      title: `Deleting resource ${resource.name}`,
+      content: `Do you really want to delete resource ${resource.name} (${resource.symbol})`
+    }).toPromise();
+
+    if (confirmation.type === "ok") {
+      this.delete(resource);
+    }
+  }
+
+  private async delete(resource: IResource) {
+    this.loadingPromise = this.resourcesService.delete(resource.id);
+
+    try {
+      await this.loadingPromise;
+      this.notificationService.create({
+        title: `Deleted ${resource.name} (${resource.symbol})`,
+        body: `Deleted ${resource.name} (${resource.symbol})`,
+        styleType: NotificationStyleType.success,
+        timeout: 8000
+      });
+      this.loadData();
+    } catch(err) {
+      this.handleError(err);
+    }
+  }
+
+  async onDeleteSelected() {
+    const confirmation = await this.alertService.confirm({
+      title: `Deleting ${this.selected.size} records...`,
+      content: `Do you really want to ${this.selected.size} resources?`
+    }).toPromise();
+
+    if (confirmation.type === "ok") {
+      this.deleteSelected();
+    }
+  }
+
+  private async deleteSelected() {
+    // TODO: Add to backend possibility of deleting multiple records... for now...
+    // TODO: This is very bad
+    this.loadingPromise = Promise.all(Array.from(this.selected)
+      .map(id => this.resourcesService.delete(id)));
+
+    try {
+      await this.loadingPromise;
+      this.loadData();
+    }
+    catch (err) {
+      this.handleError(err);
+    }
   }
 }
