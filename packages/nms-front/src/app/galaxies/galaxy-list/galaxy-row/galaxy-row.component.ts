af7af7cd92a31498b08db0c76aa17f1e3a77aeb0
ScuroGuardiano <scuroguardiano@gmail.com>
2022-08-09 00:35:52 +0000

    Galaxy row template

--- packages/nms-front/src/app/galaxies/galaxy-list/galaxy-row/galaxy-row.component.ts
+++ packages/nms-front/src/app/galaxies/galaxy-list/galaxy-row/galaxy-row.component.ts
@@ -1,45 +1,10 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { IGalaxy } from 'src/app/api-interfaces';
+import EditableRow from 'src/app/helpers/editable-row';
 
 @Component({
   selector: 'app-galaxy-row',
   templateUrl: './galaxy-row.component.html',
   styleUrls: ['./galaxy-row.component.scss']
 })
-export class GalaxyRowComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-  @Input() galaxy!: IGalaxy;
-  @Output() delete = new EventEmitter<IGalaxy>();
-  @Output() update = new EventEmitter<IGalaxy>();
-
-  editing = false;
-  edited!: IGalaxy;
-  valid = false;
-
-  onDelete() {
-    this.delete.emit(this.galaxy);
-  }
-
-  edit() {
-    this.editing = true;
-    this.edited = { ...this.galaxy };
-  }
-
-  onInput(form: HTMLFormElement) {
-    this.valid = form.checkValidity();
-  }
-
-  onSubmit() {
-    this.update.emit(this.edited);
-    this.editing = false;
-  }
-
-  onDiscard() {
-    this.editing = false;
-  }
-}
+export class GalaxyRowComponent extends EditableRow<IGalaxy> {}
